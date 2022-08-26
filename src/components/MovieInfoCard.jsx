cfc00b616f121fdcc27b133268cd506544f91ade
jo-oo <johannakoreo@gmail.com>
2022-08-26 19:10:01 +0200

    renders actors

--- src/components/MovieInfoCard.jsx
+++ src/components/MovieInfoCard.jsx
@@ -1,10 +1,15 @@
-import { Button, Card, Container } from 'react-bootstrap'
+import { Button, Card, Container, Row} from 'react-bootstrap'
+import { Link, useNavigate} from 'react-router-dom'
 //import { Link } from 'react-router-dom'
 
 const MovieInfoCard = ({ movie }) => {
+
+	const navigate = useNavigate() 
+
 	return (
 		<Container className="py-3 text-center">
-
+				
+			{/*render only if data (movie) is fetched*/}
 			 { movie && 
 
 						<Card key={movie.id} className="mb-3">
@@ -21,6 +26,28 @@ const MovieInfoCard = ({ movie }) => {
 											<div>
 												<span className="fw-bold"> Overview: </span>  {movie.overview}
 											</div>
+											
+											<div>
+											<h3>Cast</h3>
+
+											<div className='d-flex flex-wrap'>
+											<Row xs={2} md={4} lg={6} className="g-4">
+												{movie.credits.cast.map(actor => (
+													<Card key={actor.id} className='flex-fill'>
+														{actor.profile_path && (
+															<Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} />
+														)}
+														<Card.Body className='d-flex flex-column'>
+															<Card.Title>{actor.name}</Card.Title>
+															<Card.Text className='text-muted'>{actor.character}</Card.Text>
+															<Button className='mt-auto' as={Link} to={`/actor/${actor.id}`} variant="dark">Read more</Button>
+														</Card.Body>
+													</Card>
+												))}
+											</Row>
+											</div>
+												
+										</div>
 										</Card.Text>
 										<Button 
 											//as={Link}
