42f7419ad4cd25bc94e8e8b83df7e14800639d94
jo-oo <johannakoreo@gmail.com>
2022-08-26 19:17:38 +0200

    made include_adult=false into const

--- src/services/movieDbAPI.js
+++ src/services/movieDbAPI.js
@@ -3,27 +3,29 @@ import axios from 'axios'
 const API_KEY = import.meta.env.VITE_API_KEY
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
+const adultCont = "&include_adult=false";
+
 const getPopularMovies = async () => {
 	console.log("apikey:", API_KEY)
 	
-	const test = await axios.get(`movie/popular?api_key=${API_KEY}`)
+	const test = await axios.get(`movie/popular?api_key=${API_KEY}${adultCont}`)
 	console.log(test)
 	return test;	
 }
 
 const getTopRatedMovies = async () => {
-	const response = await axios.get(`movie/top_rated?api_key=${API_KEY}`)
+	const response = await axios.get(`movie/top_rated?api_key=${API_KEY}${adultCont}`)
 	console.log(response)
 	return response;
 }
 
 const getCurrentMovies = async () => {
-	return await axios.get(`movie/now_playing?api_key=${API_KEY}&include_adult=false`)
+	return await axios.get(`movie/now_playing?api_key=${API_KEY}${adultCont}`)
 }
 
 //get specific movie, based on id. 
 const getSpecificMovie = async (id) => {
-	const res = await axios.get(`movie/${id}?api_key=${API_KEY}&include_adult=false&append_to_response=credits`) //only gets credits endpoint when this is added
+	const res = await axios.get(`movie/${id}?api_key=${API_KEY}${adultCont}&append_to_response=credits`) //only gets credits endpoint when this is added
 	console.log("Response", res.data)
 	return res.data;
 }
