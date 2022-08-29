c863261c5f27abf2669287c3ca0e990ca7acf5bd
jo-oo <johannakoreo@gmail.com>
2022-08-29 17:57:50 +0200

    changed to lower case

--- src/hooks/useCurrentMovies.js
+++ src/hooks/useCurrentMovies.js
@@ -1,10 +1,10 @@
 //the useQuery hook executes queries. It uses getPopularMovies-function from the service to fetch data from the Movie DB
 
 import { useQuery } from 'react-query'
-import MovieDbAPI from '../services/MovieDbAPI'
+import movieDbAPI from '../services/movieDbAPI'
 
 const useCurrentMovies = () => {
-	return useQuery('currentMovies', MovieDbAPI.getCurrentMovies) //QueryKey=currentMovies
+	return useQuery('currentMovies', movieDbAPI.getCurrentMovies) //QueryKey=currentMovies
 }
 
 export default useCurrentMovies
