401d918f5df8a731ccb6d2068354009aedd0aa83
jo-oo <johannakoreo@gmail.com>
2022-08-29 23:13:48 +0200

    removed unnecessary code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,9 @@
 import Container from 'react-bootstrap/Container'
-import movieDbAPI from '../services/movieDbAPI' //testing response for genres (remove later)
-import { useQuery } from 'react-query' //testing response for genres (remove later)
 
 //components
 import MovieCarousel from '../components/MovieCarousel'
 
 const HomePage = () => {
-
-//testing repsonse for genre. (remove later)
-console.log("DETTA ÄR HOMEPAGE", movieDbAPI.getGenres());
-const { data, isError, error, isLoading } =  useQuery('genres', movieDbAPI.getGenres) 
 
 	return (
 		<Container className="py-3 my-3">
