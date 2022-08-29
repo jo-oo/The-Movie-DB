bc0e1bfe98f053ed433dee2d7f5019011e1f808c
jo-oo <johannakoreo@gmail.com>
2022-08-29 17:55:51 +0200

    changed to lowercase

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,10 +1,10 @@
 //the useQuery hook executes queries. It uses getPopularMovies-function from the service to fetch data from the Movie DB
 
 import { useQuery } from 'react-query'
-import MovieDbAPI from '../services/MovieDbAPI'
+import movieDbAPI from '../services/movieDbAPI'
 
 const usePopularMovies = () => {
-	return useQuery('popularMovies', MovieDbAPI.getPopularMovies) //QueryKey=popularMovies
+	return useQuery('popularMovies', movieDbAPI.getPopularMovies) //QueryKey=popularMovies
 }
 
 export default usePopularMovies
--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -1,10 +1,10 @@
 //the useQuery hook executes queries. It uses getPopularMovies-function from the service to fetch data from the Movie DB
 
 import { useQuery } from 'react-query'
-import MovieDbAPI from '../services/MovieDbAPI'
+import movieDbAPI from '../services/movieDbAPI'
 
 const useTopRatedMovies = () => {
-	return useQuery('topRatedMovies', MovieDbAPI.getTopRatedMovies) //QueryKey=topRatedMovies
+	return useQuery('topRatedMovies', movieDbAPI.getTopRatedMovies) //QueryKey=topRatedMovies
 }
 
 export default useTopRatedMovies
